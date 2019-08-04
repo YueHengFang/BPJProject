d5da7eed831ceeae604b849a93b3f8870d9de6b6
“YueHengFang” <2836486971@qq.com>
2019-08-04 17:50:20 +0800

    two

--- BPJproject/src/main/java/com/example/demo/BpJprojectApplication.java
+++ BPJproject/src/main/java/com/example/demo/BpJprojectApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BpJprojectApplication {
-
+//nihao
 	public static void main(String[] args) {
 		SpringApplication.run(BpJprojectApplication.class, args);
 	}
